d5ed96b123b5754116885a6323b84cb1714c7bd5
= <umairziyan@protonmail.com>
2024-09-16 16:24:39 +0400

    Added build script for postgres and included dependencies required for rust.

---
+++ migrations/20240916111614_create_subscriptions_table.sql
@@ -0,0 +1,8 @@
+-- Create table for subscriptions
+CREATE TABLE subscriptions (
+    id uuid NOT NULL,
+    PRIMARY KEY(id),
+    email TEXT NOT NULL UNIQUE,
+    name TEXT NOT NULL,
+    subscribed_at timestamptz NOT NULL
+);
