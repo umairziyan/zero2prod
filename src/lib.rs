c85a22ea813865adcc2a280ff272f8ec1da35e8f
= <umairziyan@protonmail.com>
2024-09-16 16:24:54 +0400

    Changed strucutre of code.

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,3 @@
-use actix_web::dev::Server;
-#[allow(unused_imports)]
-use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer, Responder};
-use std::net::TcpListener;
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String,
-}
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listener)?
-    .run();
-    // No .await here!
-    Ok(server)
-}
+pub mod configuration;
+pub mod routes;
+pub mod startup;
