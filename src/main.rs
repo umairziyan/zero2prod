ae91e83788780064ff38d01d00a1823f4f67548e
= <umairziyan@protonmail.com>
2024-09-17 11:44:48 +0400

    Added subscribe functionality as well as tests.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,17 @@
 use std::net::TcpListener;
 
-use zero2prod::configuration::{self, get_configuration};
+use sqlx::PgPool;
+use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 
 #[tokio::main]
 async fn main() -> std::io::Result<()> {
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let listener = TcpListener::bind("127.0.0.1:0").expect("Failed to bind random port");
-    let port = listener.local_addr().unwrap().port();
-    println!("http://127.0.0.1:{}", port);
-    run(listener)?.await
+    let connection_pool = PgPool::connect(&configuration.database.connection_string())
+        .await
+        .expect("Failed to connect to postgres");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(&address)?;
+    println!("{}", address);
+    run(listener, connection_pool)?.await
 }
--- src/startup.rs
+++ src/startup.rs
@@ -1,17 +1,21 @@
+use crate::routes::*;
 use actix_web::dev::Server;
 use actix_web::{web, App, HttpServer};
+use sqlx::PgPool;
 use std::net::TcpListener;
 
-use crate::routes::*;
-
-pub fn run(listener: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
+pub fn run(listener: TcpListener, db_pool: PgPool) -> Result<Server, std::io::Error> {
+    // wrap the connection in a smart pointer
+    let db_pool = web::Data::new(db_pool);
+    // capture 'connection' from the surrounding environment
+    let server = HttpServer::new(move || {
         App::new()
             .route("/health_check", web::get().to(health_check))
             .route("/subscriptions", web::post().to(subscribe))
+            // Get a pointer copy and aattach it to the application state.
+            .app_data(db_pool.clone())
     })
     .listen(listener)?
     .run();
-    // No .await here!
     Ok(server)
 }
