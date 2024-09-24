5611929db306e468342e23740814339d06375c2f
= <umairziyan@protonmail.com>
2024-09-24 13:07:41 +0400

    added ssl support

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,5 @@
+use sqlx::postgres::PgPoolOptions;
 use std::net::TcpListener;
-
-use secrecy::ExposeSecret;
-use sqlx::PgPool;
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
@@ -12,9 +10,8 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
 
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection_pool =
-        PgPool::connect_lazy(configuration.database.connection_string().expose_secret())
-            .expect("Failed to connect to postgres");
+    let connection_pool = PgPoolOptions::new().connect_lazy_with(configuration.database.with_db());
+
     let address = format!(
         "{}:{}",
         configuration.application.host, configuration.application.port
